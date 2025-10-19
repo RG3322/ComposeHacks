91b9bad1431134bc33285af49cbca424f2d7a250
RG3322 <raghavsankhyaan@gmail.com>
2025-10-19 15:44:51 +0530

    Swiping gesture

---
+++ app/src/main/java/com/fire/myapplication/ActionIcon.kt
@@ -0,0 +1,46 @@
+package com.fire.myapplication
+
+import androidx.compose.foundation.background
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton //kt-lint will want to re-order this
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun ActionIcon(modifier : Modifier= Modifier,
+               icon: ImageVector,
+               contentDescription:String?=null,
+               onClick:()->Unit,
+               backgroundColor: Color,
+               tint: Color= Color.White
+){
+
+    IconButton(
+        onClick = onClick,
+        modifier = modifier.background(backgroundColor)
+
+
+    ) {
+    Icon(
+        imageVector = icon,
+        contentDescription = contentDescription,
+        tint = tint)
+
+
+    }
+
+
+
+
+}
+/**
+ *
+ * Basic composable for testing
+ *
+ *
+ *
+ *
+ *
+ */
---
+++ app/src/main/java/com/fire/myapplication/ContactUI.kt
@@ -0,0 +1,21 @@
+package com.fire.myapplication
+
+data class ContactUI(
+    val id: Long,
+    val name: String,
+    val isOptionRevealed: Boolean = false
+) {
+
+
+
+
+
+}
+/**
+ *
+ * basic icon funtions
+ *
+ *
+ *
+ *
+ */
